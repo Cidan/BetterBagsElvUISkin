0fac9dbb03cd4f128c340a86b9d4472a37451a3c
Zhou Fang <fang2hou@outlook.com>
2024-03-03 19:33:08 +0900

    feat: add configs

--- config/defaults.lua
+++ config/defaults.lua
@@ -1,11 +1,24 @@
 local ns = (select(2, ...))
 
-ns.config = {
-    log = {
-        enabled = true,
-        level = "debug",
-    },
-    skin = {
-        enabled = true,
-    },
+local plugins_style = {
+	{ "ElvUI_ToxiUI", "toxiui" },
+	{ "ElvUI_WindTools", "windtools" },
+	{ "ElvUI_MerathilisUI", "merathilisui" },
+	{ "ElvUI_BenikUI", "benikui" },
 }
+
+local defaults = {
+	log_level = "error",
+	transparent = true,
+	cooldown = true,
+	style = (function()
+		for _, plugin_data in ipairs(plugins_style) do
+			if IsAddOnLoaded(plugin_data[1]) then
+				return plugin_data[2]
+			end
+		end
+		return "elvui"
+	end)(),
+}
+
+ns.defaults = defaults
